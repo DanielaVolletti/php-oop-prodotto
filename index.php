65a68d8c7b299087110ab1d5632c197ec20cd7a7
Daniela Maria Volletti <danielavolletti@gmail.com>
2020-06-03 18:03:10 +0200

    frutta e carne

--- index.php
+++ index.php
@@ -8,39 +8,57 @@
 
     <?php
 
-      class Scaffale {
-        public $width;
-        public $height;
-        public $ripiani;
-        public $materiale;
-        public $caricoMax;
+      class Prodotto {
+        public $nome;
+        public $prezzo;
 
-        public function __construct($width, $height, $ripiani, $materiale, $caricoMax){
-          $this -> width = $width;
-          $this -> height = $height;
-          $this -> ripiani = $ripiani;
-          $this -> materiale = $materiale;
-          $this -> caricoMax = $caricoMax;
+        public function __construct($nome, $prezzo){
+          $this -> nome = $nome;
+          $this -> prezzo = $prezzo;
+        }
+      }
+
+      class Carne extends Prodotto {
+        public $provenienza;
+
+        public function __construct($nome, $prezzo, $provenienza){
+          parent::__construct($nome, $prezzo);
+          $this -> provenienza = $provenienza;
         }
 
         public function printMe(){
-          echo "Lo scaffale è largo " . $this -> width . "m"
-              . ", è alto " . $this -> height . "m"
-              . ", ed ha " . $this -> ripiani . " ripiani; "
-              . "telaio in " . $this -> materiale
-              . ", carico massimo di " . $this -> caricoMax . "kg" . "<br>";
+          echo "Carne di " . $this -> nome
+              . ", prezzo " . $this -> prezzo . " euro al kg"
+              . ", provenienza: " . $this -> provenienza . "<br>";
+        }
+      }
+
+      class Frutta extends Carne {
+        public $colore;
+
+        public function __construct($nome, $prezzo, $provenienza, $colore){
+          parent::__construct($nome, $prezzo, $provenienza);
+          $this -> colore = $colore;
+        }
+
+        public function printMe(){
+          echo "Tipo di frutto: " . $this -> nome
+              . ", prezzo " . $this -> prezzo . " euro al kg"
+              .", proveniente da " . $this -> provenienza
+              . ", colore " . $this -> colore . "<br>";
         }
       }
 
 
+      $carne1 = new Carne("vitello", 40, "Italia");
+      $carne2 = new Carne("suino", 35, "Germania");
+      $carne1 -> printMe();
+      $carne2 -> printMe();
 
-      $scaffale1 = new Scaffale(2, 2.5 , 4, "plastica", 20);
-      $scaffale2 = new Scaffale(1, 1, 3, "legno", 8);
-      $scaffale3 = new Scaffale(4, 4, 6, "acciaio", 200);
-
-      $scaffale1 -> printMe();
-      $scaffale2 -> printMe();
-      $scaffale3 -> printMe();
+      $frutto1 = new Frutta("pesca", 3, "Italia", "gialla");
+      $frutto2 = new Frutta("arancia", 2.5, "Turchia", "arancione");
+      $frutto1 -> printMe();
+      $frutto2 -> printMe();
 
      ?>
 
